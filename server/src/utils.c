e348a91d3dbd269e79596368c824b61b4d6acbf9
utnso <sbaezduic@frba.utn.edu.ar>
2025-08-24 22:41:06 +0000

    points 1-3 working, deploying on ubuntu server next

--- server/src/utils.c
+++ server/src/utils.c
@@ -5,9 +5,9 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
-	int socket_servidor;
+	int err;
 
 	struct addrinfo hints, *servinfo, *p;
 
@@ -16,7 +16,17 @@ int iniciar_servidor(void)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	err = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+
+	int socket_servidor = socket(servinfo->ai_family,
+							servinfo->ai_socktype,
+							servinfo->ai_protocol);
+
+	err = setsockopt(socket_servidor, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
+
+	err = bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	err = listen(socket_servidor, SOMAXCONN);
 
 	// Creamos el socket de escucha del servidor
 
@@ -33,10 +43,24 @@ int iniciar_servidor(void)
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente = 0;
+	socket_cliente = accept(socket_servidor, NULL, NULL);
+	
+	size_t bytes;
+
+	int32_t handshake;
+	int32_t resultOk = 0;
+	int32_t resultError = -1;
+
+	bytes = recv(socket_cliente, &handshake, sizeof(int32_t), MSG_WAITALL);
+	if (handshake == 1) {
+		bytes = send(socket_cliente, &resultOk, sizeof(int32_t), 0);
+	} else {
+		bytes = send(socket_cliente, &resultError, sizeof(int32_t), 0);
+	}
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
